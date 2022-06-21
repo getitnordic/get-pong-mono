760419beea5cf30fbb2755a495e9aef28e8c78df
Tub <nilstobiasekman@gmail.com>
2022-06-21 13:15:16 +0200

    white text color

--- app/lib/constants/color_constants.dart
+++ app/lib/constants/color_constants.dart
@@ -4,7 +4,7 @@ class ColorConstants {
   static const Color scaffoldBackgroundColor = Color(0xff1A344A);
   static const Color primaryColor = Color(0xffF87227);
   static const Color appBarColor = Color(0xff172C3F);
-  static const Color textColor = Color(0xffd9e3ec);
+  static const Color textColor = Colors.white;
   static const Color secondaryTextColor = Color(0xff82A4C1);
   static const Color disabledButtonColor = Color(0xff8f8f90);
   static const Color dividerColor = Color(0xff82A4C1);
