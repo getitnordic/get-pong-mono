3c6051691fc20fe45f8bb99337d03bfe956e074c
jonas.granbom <jonasgranbom@hotmail.com>
2022-08-23 14:56:17 +0200

    Results are now uploaded for both players according to outcome. Values are a bit WHACK, will fix that soon

--- api/GetPong/GetPong.Infrastructure/MongoDb/ResultRepository.cs
+++ api/GetPong/GetPong.Infrastructure/MongoDb/ResultRepository.cs
@@ -12,10 +12,12 @@ public class ResultRepository : IResultRepository
 
     private readonly IMongoCollection<BsonDocument> _mongoCollection;
     private readonly IConfiguration _configuration;
+    private readonly IPlayerRepository _playerRepository;
 
-    public ResultRepository(IConfiguration configuration)
+    public ResultRepository(IConfiguration configuration, IPlayerRepository playerRepository)
     {
         _configuration = configuration;
+        _playerRepository = playerRepository;
         var mongoClient = new MongoClient(_configuration["MongoDb:ConnectionString"]);
         var mongoDatabase = mongoClient.GetDatabase("gpdb");
         _mongoCollection = mongoDatabase.GetCollection<BsonDocument>("results");
@@ -33,16 +35,62 @@ public class ResultRepository : IResultRepository
     public Task<Result> SaveResult(Game game)
     {
 
+        //Check which team won
+
+        int homeTeamScore = 0;
+        int awayTeamScore = 0;
+        bool didHomeTeamWin = false;
+
+        for (int i = 0; i < game.Sets.Count; i++)
+        {
+            homeTeamScore += game.Sets[i].HomeTeam;
+            awayTeamScore += game.Sets[i].AwayTeam;
+        }
+
+        if (homeTeamScore > awayTeamScore)
+        {
+            didHomeTeamWin = true;
+        }
+        
         // singles match
         if (game.AwayTeamIds.Count == 1)
         {
-            //TODO: Change hardcoded dummy values to real values
-            var player1Doc = new BsonDocument()
-                .Add("elo_diff", 1234)
-                .Add("new_elo", 1000 + 1234)
-                .Add("game_won", true)
-                .Add("time_stamp", DateTime.UtcNow);
-            _mongoCollection.InsertOne(player1Doc);
+
+            var player1 = _playerRepository.GetPlayerById(game.HomeTeamIds[0]);
+            var player2 = _playerRepository.GetPlayerById(game.AwayTeamIds[0]);
+
+            if (didHomeTeamWin)
+            {
+                var player1Doc = new BsonDocument()
+                    .Add("elo_diff", player1.Result.TotalScore - player2.Result.TotalScore)
+                    .Add("new_elo", player1.Result.TotalScore + 10)
+                    .Add("game_won", true)
+                    .Add("time_stamp", game.TimeStamp);
+                
+                var player2Doc = new BsonDocument()
+                    .Add("elo_diff", player2.Result.TotalScore - player1.Result.TotalScore)
+                    .Add("new_elo", player2.Result.TotalScore - 10)
+                    .Add("game_won", false)
+                    .Add("time_stamp", game.TimeStamp);
+                _mongoCollection.InsertOne(player1Doc);  
+                _mongoCollection.InsertOne(player2Doc);   
+            }
+            else
+            {
+                var player1Doc = new BsonDocument()
+                    .Add("elo_diff", player2.Result.TotalScore - player1.Result.TotalScore)
+                    .Add("new_elo", player1.Result.TotalScore - 10)
+                    .Add("game_won", false)
+                    .Add("time_stamp", game.TimeStamp);
+                
+                var player2Doc = new BsonDocument()
+                    .Add("elo_diff", player1.Result.TotalScore - player2.Result.TotalScore)
+                    .Add("new_elo", player2.Result.TotalScore + 10)
+                    .Add("game_won", true)
+                    .Add("time_stamp", game.TimeStamp);
+                _mongoCollection.InsertOne(player1Doc);  
+                _mongoCollection.InsertOne(player2Doc); 
+            }
         }
         // doubles match
         else if (game.AwayTeamIds.Count == 2)
